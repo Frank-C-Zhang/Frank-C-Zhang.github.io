8aba8e1e55fc384d93b779aecf21fb0ff56c7aca
Frank <zc_smile@outlook.com>
2025-07-09 17:43:04 +0800

    新增乐乐

--- familyday/registerSW.js
+++ familyday/registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/familyday/sw.js', { scope: '/familyday/' })})}
+if ('serviceWorker' in navigator) { window.addEventListener('load', () => { navigator.serviceWorker.register('/familyday/sw.js', { scope: '/familyday/' }) }) }
